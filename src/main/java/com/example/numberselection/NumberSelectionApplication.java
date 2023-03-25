a29682be099089141e5e4c822eaac851abfa8086
zzcyx <zzcyxp@hotmail.com>
2023-03-25 17:49:49 +0800

    环境完善

---
+++ src/main/java/com/example/numberselection/NumberSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.numberselection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberSelectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberSelectionApplication.class, args);
+    }
+
+}
